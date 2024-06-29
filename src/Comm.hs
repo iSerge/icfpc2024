48d27d539b490a13f78c8b53f9d654749c4718bc
Serge Ivanov <lutvuk@gmail.com>
2024-06-29 18:36:49 +0300

    Small Comm module refactoring

--- src/Comm.hs
+++ src/Comm.hs
@@ -1,6 +1,5 @@
 module Comm(encode, communicate) where
 
-import Term(toICFP)
 import Base94(s2icfp)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
@@ -8,11 +7,9 @@ import Network.HTTP.Types.Header(Header, hAuthorization)
 import Network.HTTP.Types.Method(methodPost)
 import Data.ByteString.UTF8 as BSU 
 import Data.ByteString.Lazy as BL 
-import Data.ByteString.Lazy.UTF8 as BLU 
 
 import Data.Text (Text)
 import qualified Data.Text as Text
-import qualified Data.Text.Lazy as TL
 import Data.Text.Encoding as TSE
 
 authToken :: Header
@@ -21,7 +18,7 @@ authToken = (hAuthorization , BSU.fromString "Bearer a57888ca-06a7-492c-8439-8c9
 commUrl :: String
 commUrl = "https://boundvariable.space/communicate"
 
-encode :: Text->BSU.ByteString
+encode :: Text -> BSU.ByteString
 encode s =  BSU.fromString $ 'S' : (s2icfp $ Text.unpack s)
 
 communicate :: Text -> IO Text
@@ -30,4 +27,4 @@ communicate s = do
     initialRequest <- parseRequest commUrl
     let request = initialRequest { method = methodPost, requestBody = RequestBodyBS $ encode s, requestHeaders = [authToken] }
     response <- httpLbs request manager
-    return $ TSE.decodeLatin1 . BL.toStrict $ responseBody response
+    return $ TSE.decodeUtf8 . BL.toStrict $ responseBody response
